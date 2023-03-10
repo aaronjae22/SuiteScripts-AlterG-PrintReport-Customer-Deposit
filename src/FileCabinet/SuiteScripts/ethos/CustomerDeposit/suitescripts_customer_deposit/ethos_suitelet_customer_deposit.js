bd7115e961c0386481e943f6bbb7c4b4d31e552c
Aaron Ayerdis Espinoza <aaronaej7@gmail.com>
2023-03-10 13:03:45 -0600

    7. Getting Primary Information from Customer Deposit Record in Netsuite

--- src/FileCabinet/SuiteScripts/ethos/CustomerDeposit/suitescripts_customer_deposit/ethos_suitelet_customer_deposit.js
+++ src/FileCabinet/SuiteScripts/ethos/CustomerDeposit/suitescripts_customer_deposit/ethos_suitelet_customer_deposit.js
@@ -47,6 +47,40 @@ define(['N/file', 'N/format', 'N/https', 'N/query', 'N/record', 'N/render', 'N/r
             if (!params.tpl)
                 return ;
 
+            const customerDeposit = record.load({id: recId, type: recType, isDynamic: true});
+            // log.debug({title: 'Record', details: customerDeposit});
+
+            // Getting Payment Events List Info from Netsuite //
+            const paymentEventsListName = 'paymentevent';
+            const lineCount = customerDeposit.getLineCount({sublistId: paymentEventsListName});
+
+            // Getting Customer Deposit Primary Info //
+            const customer = customerDeposit.getValue({fieldId: 'entityname'});
+            // const salesOrder = customerDeposit.getValue({fieldId: 'salesorder'});
+            const salesOrder = customerDeposit.getText({fieldId: 'salesorder'});
+            const deposit = customerDeposit.getValue({fieldId: 'tranid'});
+            const paymentAmount = customerDeposit.getValue({fieldId: 'payment'});
+            const currency = customerDeposit.getValue({fieldId: 'currencyname'});
+            // const transactionDate = customerDeposit.getValue({fieldId: 'trandate'});
+            const transactionDate = format.format({type: format.Type.DATE, value: customerDeposit.getValue({fieldId: 'trandate'})});
+            const postingPeriod = customerDeposit.getText({fieldId: 'postingperiod'});
+            const account = customerDeposit.getValue({fieldId: 'account'});
+            const memo = customerDeposit.getValue({fieldId: 'memo'});
+
+            const recData = {
+                customer,
+                salesOrder,
+                deposit,
+                paymentAmount,
+                currency,
+                transactionDate,
+                postingPeriod,
+                account,
+                memo
+            };
+
+            log.debug({title: 'Record Data', details: recData});
+
         }
 
         return {onRequest}
